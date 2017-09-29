3ec94392eaafcc79bcb2f2ef050bbf11d5510935
Scarlet79sp <nohhipurasu001@yahoo.co.jp>
2017-09-29 09:41:14 +0900

    EffectManager

---
+++ 53Team/Assets/Script/Effect/EffectMan.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectMan : MonoBehaviour {
+
+    private static EffectMan m_instance = null;
+
+    private void Awake()
+    {
+        if(m_instance == null)
+        {
+            m_instance = this;
+        }
+    }
+
+    public static EffectMan Instance
+    {
+        get { return m_instance; }
+        private set { m_instance = value; }
+    }
+
+}
---
+++ 53Team/Assets/Script/Effect/NormalBullet.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UniRx;
+using UniRx.Toolkit;
+
+public class NormalBullet : MonoBehaviour {
+
+    private Transform m_startTransform;
+    private Transform m_targetTransform;
+
+    public IObservable<Unit> Shot(Transform aPos, Transform aTarget)
+    {
+        m_startTransform = aPos;
+        m_targetTransform = aTarget;
+
+        return Observable.ReturnUnit();
+    }
+}
+
+public class NormalBulletPool : ObjectPool<NormalBullet>
+{
+    private readonly NormalBullet _prefab;
+    private readonly Transform _parentTransform;
+
+    public NormalBulletPool(NormalBullet prefab, Transform parent = null)
+    {
+        _prefab = prefab;
+        _parentTransform = parent;
+    }
+
+    protected override NormalBullet CreateInstance()
+    {
+        var go = GameObject.Instantiate(_prefab, _parentTransform);
+
+        return go;
+    }
+
+    protected override void OnBeforeRent(NormalBullet instance)
+    {
+        base.OnBeforeRent(instance);
+    }
+
+    protected override void OnBeforeReturn(NormalBullet instance)
+    {
+        base.OnBeforeReturn(instance);
+    }
+}
