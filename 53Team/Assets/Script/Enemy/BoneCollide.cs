9f80124a978b771c42696f551c5d7a532ce3f8f3
Scarlet79sp <nohhipurasu001@yahoo.co.jp>
2017-10-20 14:03:08 +0900

    部位ごとの判定とダメージ

---
+++ 53Team/Assets/Script/Enemy/BoneCollide.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class BoneCollide : MonoBehaviour {
+
+    public CharaBase.Parts m_parts;
+
+    public Subject<int> OnDamage = new Subject<int>();
+
+    public void Damage(int aAttckValue)
+    {
+        OnDamage.OnNext(aAttckValue);
+    }
+}
---
+++ 53Team/Assets/Script/Enemy/hogehoge/HogeRayShot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HogeRayShot : MonoBehaviour {
+
+    public Camera m_camera;
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = m_camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+            if(Physics.Raycast(ray, out hit))
+            {
+                hit.collider.gameObject.GetComponent<BoneCollide>().Damage(10);
+            }
+        }
+	}
+}
