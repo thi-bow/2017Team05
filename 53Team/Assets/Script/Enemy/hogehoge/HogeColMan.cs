7d3ce61706fd02b8d93f429c0afbbdefbf74f222
Scarlet79sp <nohhipurasu001@yahoo.co.jp>
2017-10-24 13:25:34 +0900

    hoge

--- 53Team/Assets/Script/Enemy/hogehoge/HogeColMan.cs
+++ 53Team/Assets/Script/Enemy/hogehoge/HogeColMan.cs
@@ -9,15 +9,22 @@ public class HogeColMan : CharaBase {
     [Header("各部位の当たり判定")]
     public BoneCollide[] m_boneCollides;
 
+    [Header("デバッグ用パーツ")]
+    public Armor m_armor;
+
     protected override void Start()
     {
+        if (m_armor != null) { PartsAdd(Parts.RightArm, m_armor); }
+
+
         for (int i = 0; i < m_boneCollides.Length; i++)
         {
             int n = i;
             m_boneCollides[n].OnDamage.Subscribe(dmg => 
             {
-                Debug.LogFormat("Hit!!!!!!  Parts.{0} {1}damage", m_boneCollides[n].m_parts.ToString(), dmg);
-                PartsDamage(dmg, m_boneCollides[n].m_parts);
+                Parts parts = m_boneCollides[n].m_parts;
+                Debug.LogFormat("Hit!!!!!!  Parts.{0} {1}damage", parts.ToString(), dmg);
+                PartsDamage(dmg, parts, () => Debug.Log(parts + "パージ!!"));
             });
         }
     }
