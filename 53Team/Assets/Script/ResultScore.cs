7cfb49ed7453d6c40641b2c28e3a13e57bfb7056
thi-bow <ground_thi_bo@yahoo.co.jp>
2017-11-07 15:17:35 +0900

    キャラベースの攻撃の更新

---
+++ 53Team/Assets/Script/ResultScore.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultScore : MonoBehaviour
+{
+    public static int KillCount = 0;
+    public static int ShotKillCount = 0;
+    public static int ApproachKillCount = 0;
+    public static int DefaultKillCount = 0;
+
+    public static void Reset()
+    {
+        KillCount = 0;
+        ShotKillCount = 0;
+        ApproachKillCount = 0;
+        DefaultKillCount = 0;
+    }
+}
