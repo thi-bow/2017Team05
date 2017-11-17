79e96d6ab864beb2428829f6b5fcec36d26178f8
Scarlet79sp <nohhipurasu001@yahoo.co.jp>
2017-11-17 10:51:58 +0900

    カメラ修正ちゅう

--- 53Team/Assets/Script/Camera/TPS_Camera.cs
+++ 53Team/Assets/Script/Camera/TPS_Camera.cs
@@ -6,10 +6,14 @@ public class TPS_Camera : MonoBehaviour {
 
     [Header("でばっく")]
     public bool ray;
+    public Vector3 hoge;
+    public Vector3 rotate;
+
 
     [Space(10)]
     public bool m_run;                  // マウス追従フラグ
 
+    public Camera m_camera;
     [Range(1, 100)]
     public float m_value = 50.0f;       // マウス感度
     public float m_distance = 5.0f;     // カメラとTargetの目標距離
@@ -37,35 +41,46 @@ public class TPS_Camera : MonoBehaviour {
 
         if (m_run)
         {
-            m_vec1 = (transform.position - m_targetObj.transform.position).normalized;
-
             UpdateCamMove();
+            UpdateCamRotate();
         }
+    }
+
+    private void UpdateCamRotate()
+    {
+        float mouseInputX = Input.GetAxis("Mouse X");
+        float mouseInputY = -Input.GetAxis("Mouse Y");
+
+        rotate.x = mouseInputX * Time.deltaTime * 10f * m_value;
+        rotate.y = mouseInputY * Time.deltaTime * 10f * m_value;
+
+        // targetの位置のY軸を中心に、回転(公転)する
+        transform.RotateAround(transform.position, Vector3.up, rotate.x);
+        // カメラの垂直移動（角度制限なし）
+        transform.RotateAround(transform.position, transform.right, rotate.y);
+
+
+
+        m_camera.transform.LookAt(m_currentPos);
     }
 
     private void UpdateCamMove()
     {
         // targetの移動量分、カメラも移動する
         m_currentPos = m_targetObj.transform.position + m_offset;
-        transform.position += m_currentPos - m_targetPos;
-        m_targetPos = m_currentPos;
-
-        float mouseInputX = Input.GetAxis("Mouse X");
-        float mouseInputY = -Input.GetAxis("Mouse Y");
-
-        // targetの位置のY軸を中心に、回転(公転)する
-        transform.RotateAround(m_targetPos, Vector3.up, mouseInputX * Time.deltaTime * 10f * m_value);
-        // カメラの垂直移動（角度制限なし）
-        transform.RotateAround(m_targetPos, transform.right, mouseInputY * Time.deltaTime * 10f * m_value);
+        //transform.position += m_currentPos - m_targetPos;
+        m_vec1 = (m_camera.transform.position - m_currentPos).normalized;
+        //m_targetPos = m_currentPos;
 
         if (IsRayCast())
         {
-            transform.position = Vector3.Lerp(transform.position, m_hit.point, Time.deltaTime);
+            m_camera.transform.position = Vector3.Lerp(m_camera.transform.position, m_hit.point, Time.deltaTime);
         }
         else
         {
             var p = m_currentPos + m_vec1 * m_distance;
-            transform.position = Vector3.Lerp(transform.position, p, Time.deltaTime);
+            hoge = p;
+            m_camera.transform.position = Vector3.Lerp(m_camera.transform.position, p, Time.deltaTime);
         }
     }
 
@@ -76,14 +91,24 @@ public class TPS_Camera : MonoBehaviour {
         return hit;
     }
 
+    public Camera GetTPSCamera()
+    {
+        return m_camera;
+    }
+
     private void OnDrawGizmosSelected()
     {
+        if (m_camera == null || m_targetObj == null) return;
+
         Gizmos.color = Color.red;
-        Vector3 p1, p2, v1;
+        Vector3 p1, p2, p3, v1, v2;
         p1 = m_targetObj.transform.position + m_offset;
-        v1 = (transform.position - p1).normalized;
+        v1 = (m_camera.transform.position - p1).normalized;
         p2 = p1 + v1 * m_distance;
+        p3 = m_camera.transform.position;
+        v2 = m_camera.transform.forward;
         Gizmos.DrawLine(p1, p2);
+        Gizmos.DrawRay(new Ray(p3, v2));
         Gizmos.DrawWireSphere(p1, 0.2f);
         Gizmos.DrawWireSphere(p2, 0.5f);
     }
