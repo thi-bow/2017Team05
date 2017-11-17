80e3e702b3ca0a792ca6479aad4477637a58bf45
KouOka <zakiltayes334@gmail.com>
2017-11-17 14:46:15 +0900

    近接攻撃(仮)

---
+++ 53Team/Assets/Script/Weapon/ApproachAttack.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApproachAttack : MonoBehaviour {
+
+    // 近接攻撃中か
+    [SerializeField] private bool isApproach = false;
+
+    [SerializeField] private float atkTime = 1.0f;
+
+    RaycastHit hit;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Approach();
+        }
+    }
+
+    // 近接攻撃
+    public void Approach()
+    {
+        if (!isApproach)
+        {
+            isApproach = true;
+            StartCoroutine(ApproachRun());
+        }
+    }
+
+    IEnumerator ApproachRun()
+    {
+        Debug.Log("近接");
+
+        Vector3 crePos = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+
+        if (Physics.SphereCast(crePos, 0.5f, transform.forward, out hit, 1.0f))
+        {
+            Debug.Log(hit.collider.name);
+        }
+
+        yield return new WaitForSeconds(atkTime);
+
+        Debug.Log("終了");
+
+        isApproach = false;
+    }
+}
