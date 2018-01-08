b80b0bc617e51138697faaf9319df0672f549af3
thi-bow <ground_thi_bo@yahoo.co.jp>
2018-01-09 00:07:14 +0900

    Tutorialの修正・パージしたときに死亡するバグの修正

--- 53Team/Assets/Script/Player/PargeAttackCollider.cs
+++ 53Team/Assets/Script/Player/PargeAttackCollider.cs
@@ -8,38 +8,56 @@ public class PargeAttackCollider : MonoBehaviour {
     bool _parge = false;
     int _attackPower = 1000;
     float _collderSize = 5.0f;
+    float radius = 0.0f;
 	
 	// Update is called once per frame
 	void Update ()
     {
         if (_parge)
         {
-            this.GetComponent<SphereCollider>().radius += Time.deltaTime * sizeUpspeed;
-            if (this.GetComponent<SphereCollider>().radius >= _collderSize)
+            RaycastHit hit;
+            if (Physics.SphereCast(transform.position, radius, transform.forward, out hit))
+            {
+                if (hit.collider.GetComponent<BoneCollide>() != null && hit.collider.tag != this.tag)
+                {
+                    Debug.Log(hit.collider.name + "：" + _attackPower);
+                    hit.collider.gameObject.GetComponent<BoneCollide>().Damage(_attackPower, Weapon.Attack_State.approach);
+                }
+            }
+
+            radius += Time.deltaTime * sizeUpspeed;
+            if (radius >= _collderSize)
             {
                 _parge = false;
-                this.gameObject.SetActive(false);
+                gameObject.SetActive(false);
+                return;
             }
         }
-		
-	}
+
+
+    }
 
     public void PargeStart(int power, float collderSize)
     {
-        this.GetComponent<SphereCollider>().radius = 0.5f;
+        radius = 0.5f;
         _attackPower = power;
         _collderSize = collderSize;
         _parge = true;
     }
 
-    private void OnTriggerStay(Collider other)
+    //private void OnTriggerStay(Collider other)
+    //{
+
+    //    if (other.gameObject.GetComponent<BoneCollide>() != null && other.tag != this.tag)
+    //    {
+    //        Debug.Log(other.gameObject.name + "ほげほげ");
+    //        //other.gameObject.GetComponent<BoneCollide>().Damage(_attackPower, Weapon.Attack_State.NULL);
+    //    }
+    //}
+
+    private void OnDrawGizmos()
     {
 
-        if (other.gameObject.GetComponent<BoneCollide>() != null && other.tag != this.tag)
-        {
-            Debug.Log(other.gameObject.name + "ほげほげ");
-            //other.gameObject.GetComponent<BoneCollide>().Damage(_attackPower, Weapon.Attack_State.NULL);
-        }
     }
 
 
